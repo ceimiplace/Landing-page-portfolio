1d08373d2a76dc5fb059d41141c1fa4921fc61ec
ceimiplace <azamfiredenis@yahoo.com>
2022-10-11 15:10:31 +0300

    added router to wrk on dif pages UNSPLASHapi

--- src/App.js
+++ src/App.js
@@ -7,18 +7,31 @@ import Main from "./Main";
 import Navigation from "./Navigation";
 import Home from "./Components/HomePage/Home";
 import UnsplashAPI from "./Components/Projects/UnsplashAPI";
-
+import { useNavigate } from "react-router-dom";
 function App() {
+  const navigate = useNavigate();
+  function unsplashAPI(data) {
+    navigate(data);
+  }
   return (
     <div className="min-h-screen mx-auto max-w-screen-2xl flex flex-col  px-4">
       <Navigation />
-
+      <button onClick={() => navigate("projects/UnsplashAPI/2")}>
+        CLICK ME
+      </button>
       <Main>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="projects" element={<Projects />}></Route>
           <Route path="projects/perfume" element={<Perfume />} />
-          <Route path="projects/UnsplashAPI" element={<UnsplashAPI />} />
+          <Route
+            path="projects/UnsplashAPI"
+            element={<UnsplashAPI changenavi={unsplashAPI} />}
+          />
+          <Route
+            path="projects/UnsplashAPI/:id"
+            element={<UnsplashAPI changenavi={unsplashAPI} />}
+          />
         </Routes>
       </Main>
 
--- src/Components/Projects/UnsplashAPI.js
+++ src/Components/Projects/UnsplashAPI.js
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "react";
 import SearchBar from "./UnsplashApi-components/SearchBar";
 import ItemsList from "./UnsplashApi-components/ItemsList";
-export default function UnsplashAPI() {
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+export default function UnsplashAPI({ changenavi }) {
+  let { id } = useParams();
+  if (!id) {
+    id = 1;
+  }
   const [images, setImages] = useState([]);
   const [term, setTerm] = useState("cats");
-  const [page, setPage] = useState(1);
+
   useEffect(() => {
     console.log(term);
-    fetch(`https://api.unsplash.com/search/photos?query=${term}&page=${page}`, {
+    fetch(`https://api.unsplash.com/search/photos?query=${term}&page=${id}`, {
       method: "GET",
       headers: {
         "Content-type": "application/json",
@@ -19,21 +25,18 @@ export default function UnsplashAPI() {
         console.log(dates);
         setImages(dates.results);
       });
-  }, [term, page]);
-  console.log(page);
+  }, [term, id]);
+
   return (
     <div className="grow w-4/6 flex flex-col  p-3  mx-auto">
       <SearchBar submited={setTerm} initialTerm={term} />
       <ItemsList images={images} />
       <div className="flex justify-center gap-4">
-        <div
-          onClick={() => setPage((prev) => prev - 1)}
-          className="border-2 border-black p-4 text-lg rounded"
-        >
+        <div className="border-2 border-black p-4 text-lg rounded">
           Previous page
         </div>
         <div
-          onClick={() => setPage((prev) => prev + 1)}
+          onClick={() => changenavi(`projects/UnsplashAPI/${Number(id) + 1}`)}
           className="border-2 border-black p-4 text-lg rounded"
         >
           Next page
