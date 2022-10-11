18bdbe28a027879b6be2e3c02790d38b629c9026
ceimiplace <azamfiredenis@yahoo.com>
2022-10-11 16:11:41 +0300

    Update ItemsList.js

--- src/Components/Projects/UnsplashApi-components/ItemsList.js
+++ src/Components/Projects/UnsplashApi-components/ItemsList.js
@@ -1,14 +1,16 @@
 export default function ItemsList({ images }) {
   return (
-    <div className="columns-2xs xl:columns-sm">
-      {images.map((image) => (
-        <img
-          src={image.urls.regular}
-          key={image.id}
-          className="m-2"
-          alt="image"
-        />
-      ))}
-    </div>
+    images && (
+      <div className="columns-2xs xl:columns-sm">
+        {images.map((image) => (
+          <img
+            src={image.urls.regular}
+            key={image.id}
+            className="m-2"
+            alt="image"
+          />
+        ))}
+      </div>
+    )
   );
 }
