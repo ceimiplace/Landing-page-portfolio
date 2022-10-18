f78364d25c3a132a5a1e53618637bf46701aaf3f
ceimiplace <100032730+ceimiplace@users.noreply.github.com>
2022-10-18 16:51:08 +0300

    added headers

--- src/Components/Projects/ElearnLanding.js
+++ src/Components/Projects/ElearnLanding.js
@@ -1,17 +1,32 @@
 import "./ElearnLanding.css";
 export default function ElearnLanding() {
   return (
-    <div className="elearn-container">
-      <div className="">
-        <p className="">skilled</p>
+    <div className="elearn-container my-4">
+      <div className="flex justify-between items-baseline">
+        <p className="header-s">skilled</p>
         <Button
-          className={"dark-color-background white-color-text"}
+          className={"dark-color-background white-color-text   bodyS"}
           text={"Get Started"}
-        ></Button>
+        />
+      </div>
+      <div>
+        <p className="header-l dark-color-text my-4 ">
+          Maximize skill, minimize budget
+        </p>
+        <p className="bodyS font-light grey-color my-4">
+          Our modern courses across a range of in-demand skills will give you
+          the knowledge you need to live the life you want.
+        </p>
+        <Button
+          className={`white-color-text pink-linear-gradient my-4`}
+          text={"Get Started"}
+        />
       </div>
     </div>
   );
 }
 function Button({ className, text }) {
-  return <button className={`${className} p-4 rounded-full`}>{text}</button>;
+  return (
+    <button className={`${className} p-4 rounded-full w-40`}>{text}</button>
+  );
 }
