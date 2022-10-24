260c31cd7beccf46ab198b1f71c7a1a84fa3c1ac
ceimiplace <100032730+ceimiplace@users.noreply.github.com>
2022-10-24 20:47:30 +0300

    Update Section.js

--- src/Components/ProjectsPage/Section.js
+++ src/Components/ProjectsPage/Section.js
@@ -1,7 +1,7 @@
 import "./Section.css";
 export default function Section({ title, children }) {
   return (
-    <div className=" px-20 py-10 ">
+    <div className=" px-2 py-10 ">
       <p className="text-3xl text-[#DAA520] mb-4 ">{title}</p>
       <div className="section-container">{children}</div>
     </div>
